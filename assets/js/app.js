c5a5c942e84a9976b0cfe2fed65af522c8d1c092
Theresa Leonard <theresa@fiddleface.com>
2016-01-21 10:47:33 -0800

    fixed nav container, added boxes on home
    Signed-off-by: Theresa Leonard <theresa@fiddleface.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -289,3 +289,47 @@ var App = function () {
 	};
 
 }();
+
+
+$(document).ready(function() {
+
+
+
+  $("div.season-box").hover(
+    function() {
+        $(this).find("div.content-hide").slideToggle("fast");
+    },
+    function() {
+        $(this).find("div.content-hide").slideToggle("fast");
+    }
+  );
+
+  $('.flexslider').flexslider({
+		prevText: '',
+		nextText: ''
+	});
+
+  $('.testimonails-slider').flexslider({
+    animation: 'slide',
+    slideshowSpeed: 5000,
+    prevText: '',
+    nextText: '',
+    controlNav: false
+  });
+
+  $(function(){
+
+  // Instantiate MixItUp:
+
+  $('#Container').mixItUp();
+
+
+
+  $(document).ready(function() {
+      $(".fancybox").fancybox();
+    });
+
+  });
+
+
+});
